ae1d6307eb6d34365bb5252193171497c0d64e35
Anzo Gabriel <anzo.arajo@estudante.ifto.edu.br>
2025-09-01 13:35:21 -0300

    aLTERAÇÃO LINK

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ function App() {
     <div className="App">
       <main>
         <section className="content my-8 p-4 bg-neutral-800 text-white rounded-lg max-w-3xl mx-auto text-center">
-          <h2 className="text-xl font-semibold">Conteúdo: Processo Passo a Passo</h2>
+          <h2 className="text-xl font-semibold">Conteúdo: Benefícios das Lentes em Resina</h2>
           
 
           <div className="flex justify-center space-x-4">
-            <a href='https://drive.google.com/drive/folders/1Ek2bClU-YWuizBf_oOS0bNYg-N9jVjxo?usp=sharing' className="mt-4 inline-flex items-center gap-2 bg-orange-400 hover:bg-blue-60 font-bold py-2 px-4 rounded" style={{color: '#FFFFFF'}}>
+            <a href='https://drive.google.com/drive/folders/1Y4HJr7Lx03DwiEBNPsDW1hM6Nu4Ba4qh?usp=sharing' className="mt-4 inline-flex items-center gap-2 bg-orange-400 hover:bg-blue-60 font-bold py-2 px-4 rounded" style={{color: '#FFFFFF'}}>
               <GoogleDriveLogo size={32} /> Acessar Arquivos
             </a>
           </div>
